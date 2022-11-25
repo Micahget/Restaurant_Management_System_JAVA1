f87591bf89c85cf523f9a9048ecf1dc9bd89a7a9
Mikkiyas Getachew <mikiyasgetachew77@gmail.com>
2022-11-25 15:31:34 +0530

    Add: Color specification Add: method to get input from user anytime needed

--- Commmons/Utilities.java
+++ Commmons/Utilities.java
@@ -1,9 +1,12 @@
 package Commmons;
 
 import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class Utilities {
-    // Colors
+    public static Scanner sc = new Scanner(System.in);
+    // Colors specification
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
@@ -11,7 +14,11 @@ public class Utilities {
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
-    public static String countryFile="Ethiopia";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_INDIGO = "\u001B[34m";
+    public static final String ANSI_VIOLET = "\u001B[35m";
+    public static String countryFile = "Ethiopia";
 
     // Method to clear the console screen
     public static void clearScreen() {
@@ -21,4 +28,49 @@ public class Utilities {
             e.printStackTrace();
         }
     }
+
+    // method to recieve input from user choice any place it wanted
+    public static int validateAndReturnUserInput() {
+
+        int choice;
+        try {
+
+            // Read user input
+            System.out.print(ANSI_VIOLET + "\n\t\t\t=> Enter your choice: " + ANSI_RESET);
+            choice = sc.nextInt();
+            sc.nextLine(); // Consume newline left-over
+
+            return choice;
+
+            // Handle ctrl + d exception
+        } catch (NoSuchElementException ex) {
+
+            // Clear the console screen
+            clearScreen();
+
+            // Print goodbye message
+            System.out.println(ANSI_RED + "\n\t\t\t => Thank you for using our service" + ANSI_RESET);
+
+            // Exit the program
+            System.exit(1);
+            return -12341;
+
+        } catch (Exception e) {
+
+            // Clear the console screen
+            clearScreen();
+
+            // Print error message
+            e.printStackTrace();
+
+            // Consume the newline left-over
+            sc.nextLine();
+
+            // Return -1234 to indicate an error
+            System.out.print(Utilities.ANSI_RED + "\t\t\t => Invalid Choice" +
+                    Utilities.ANSI_RESET);
+            return -1234;
+        }
+    }
+
 }
